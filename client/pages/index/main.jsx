312c5a163b02b474d06d9aa7856faa4d3e9e4cdc
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2022-09-18 16:17:41 +0300

    dynamic import

--- client/pages/index/main.jsx
+++ client/pages/index/main.jsx
@@ -1,12 +1,33 @@
 // Import global npm modules
 import React from 'react'
+import dynamic from 'next/dynamic';
 
 import HomeSection from './ui/home/homeSection'
-import AboutSection from './ui/about/aboutSection'
-import MediaSection from './ui/media/mediaSection'
-import NewsSection from './ui/news/newsSection'
-import FormSection from './ui/form/formSection'
-import ApplicationsSection from './ui/applications/applicationsSection'
+// import AboutSection from './ui/about/aboutSection'
+// import MediaSection from './ui/media/mediaSection'
+// import NewsSection from './ui/news/newsSection'
+// import FormSection from './ui/form/formSection'
+// import ApplicationsSection from './ui/applications/applicationsSection'
+
+const AboutSection = dynamic(() => import('./ui/about/aboutSection'), {
+  ssr: false,
+});
+
+const MediaSection = dynamic(() => import('./ui/media/mediaSection'), {
+  ssr: false,
+});
+
+const NewsSection = dynamic(() => import('./ui/news/newsSection'), {
+  ssr: false,
+});
+
+const FormSection = dynamic(() => import('./ui/form/formSection'), {
+  ssr: false,
+});
+
+const ApplicationsSection = dynamic(() => import('./ui/applications/applicationsSection'), {
+  ssr: false,
+});
 
 // Create Index main
 const Index = () => {
