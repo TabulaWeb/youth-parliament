064c7bbeabfad2ea12d22b3e59a22c0234c4beeb
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2023-01-20 12:37:37 +0300

    add component loader

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -8,7 +8,6 @@ module.exports = () => {
             "https://api.vercel.com/v1/integrations/deploy/prj_40wj75dr9ls6nmxZt6mUNe1FqdAI/R34GI4aBpN",
           apiToken: "HQ7gj77hkXQuZXxoLgGwkZDe",
           appFilter: "youth-parliament-wuyk",
-          teamFilter: "tabulaweb",
           roles: ["strapi-super-admin"],
         },
       }
