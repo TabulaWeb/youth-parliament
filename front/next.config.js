c1eaa9957f0a8736ec830512c26e138623593400
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2023-01-24 00:23:11 +0300

    config

--- front/next.config.js
+++ front/next.config.js
@@ -3,14 +3,8 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // matching all API routes
-        source: "http://85.193.90.213/strapi/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
+        source: '/api/:path*',
+        destination: 'http://85.193.90.213/strapi/api/:path*',
       }
     ]
   },
