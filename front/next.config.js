cdaa5e3d180053f3ada0379ce4a5b192ecd61864
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2023-01-24 00:38:09 +0300

    Update next.config.js

--- front/next.config.js
+++ front/next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://85.193.90.213/strapi/api/:path*',
-      }
-    ]
-  },
   reactStrictMode: true,
   images: { domains: ['images.unsplash.com', 'localhost', 'mp60.ru'] }
 }
