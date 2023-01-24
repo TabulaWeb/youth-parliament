527f3e53d8815b283996f67ee2532faeb3953609
Alex Tabula <67907195+TabulaWeb@users.noreply.github.com>
2023-01-24 16:34:52 +0300

    fix

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -12,7 +12,7 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      header: '*',
+      headers: '*',
       origin: ['http://localhost:1337', 'http://85.193.90.213', 'https://mp60.ru', '*']
     }
   },
